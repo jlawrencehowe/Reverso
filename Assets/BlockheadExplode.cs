ee6234a07896aa4e8aaee9d27b8cd5311adfa91f
jlawrencehowe <jlawrencehowe@hotmail.com>
2017-08-01 16:41:14 -0400

    finish init

---
+++ Assets/BlockheadExplode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockheadExplode : MonoBehaviour {
+
+    Material mat;
+
+    Color startCol;
+    Color endCol;
+
+    float currentTime;
+    public float maxTime = 4f;
+	// Use this for initialization
+	void Start () {
+
+        mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
+        startCol = mat.color;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+       // mat.SetColor("_Color",Color.red);
+	
+	}
+}
